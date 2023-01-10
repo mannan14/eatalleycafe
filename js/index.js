421447d920e51988dcced1f945c7fa69a0248676
mannan dassani <49394950+mannan14@users.noreply.github.com>
2023-01-10 13:25:54 +0530

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,9 +47,4 @@ var form = document.getElementById("my-form");
     });
     }
     form.addEventListener("submit", handleSubmit)
-    window.onload = function() { 
-  var el = document.getElementById('g-recaptcha-response'); 
-  if (el) { 
-    el.setAttribute('required', 'required'); 
-  } 
-}
+    
